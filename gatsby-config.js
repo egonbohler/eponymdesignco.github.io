923dd6edd6a3c82e625669c57fa1aa9c1708c087
Egon Bohler <ebohler7@gmail.com>
2019-06-26 14:43:06 -0400

    Change manifest icon to .ico
    Change manifest icon to .ico

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
-        icon: "src/img/logo_main.svg", // This path is relative to the root of the site.
+        icon: "public/favicon.ico", // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
